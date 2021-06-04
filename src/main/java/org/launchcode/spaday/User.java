deb4a7846f8b6006a90a6b8bd4de6cb544e9bbfc
clairepatt19@gmail.com <clairepatt19@gmail.com>
2021-06-04 15:49:12 -0500

    worked on in class

---
+++ src/main/java/org/launchcode/spaday/User.java
@@ -0,0 +1,37 @@
+package org.launchcode.spaday;
+
+public class User {
+    private static String username;
+    private static String email;
+    private static String password;
+
+    public User() {
+        this.username=username;
+        this.email=email;
+        this.password=password;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        User.username = username;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        User.email = email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+}
